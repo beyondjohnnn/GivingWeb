dfdf5e607159d99491f4328dd8db5cc7bce4571f
FALLENau <reece.c.jones@gmail.com>
2017-09-18 18:23:27 +0100

    almost there

--- api/app/controllers/v1/charities_controller.rb
+++ api/app/controllers/v1/charities_controller.rb
@@ -1,2 +1,7 @@
 class V1::CharitiesController < ApplicationController
+  def index
+    @charities = Charities.all
+
+    render :json => @charities, status: :ok
+  end
 end
--- api/app/controllers/v1/users_controller.rb
+++ api/app/controllers/v1/users_controller.rb
@@ -1,2 +1,7 @@
 class V1::UsersController < ApplicationController
+  def index
+    @users = Users.all
+
+    render :json => @users, status: :ok
+  end
 end
