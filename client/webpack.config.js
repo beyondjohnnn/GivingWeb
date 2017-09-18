337b3cb89c52fd17eceeaff837bd5ca222b6f42b
eoghanscrowley <eoghanscrowley@gmail.com>
2017-09-18 14:14:15 +0100

    create webpack config

---
+++ client/webpack.config.js
@@ -0,0 +1,46 @@
+const config = {
+	entry: `${__dirname}/src/giving-web.js`,
+	output: {
+		path: `${__dirname}/build`,
+		filename: 'bundle.js'
+	},
+	devtool: 'source-map',
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['react', 'es2015']
+				}
+			},
+			{
+        test: /\.scss$/,
+        use: [
+	        {
+	          loader: "style-loader" // creates style nodes from JS strings
+	        }, 
+	        {
+	          loader: "css-loader" // translates CSS into CommonJS
+	        }, 
+	        {
+	          loader: "sass-loader" // compiles Sass to CSS
+	        }
+
+        ]
+      },
+      {
+        test: /\.(png|jpg|ttf)$/,
+        use: [
+         	{ loader: 'url-loader', options: { limit: 5000192 } }
+        ]
+      }
+		]
+	}
+}
+
+module.exports = config
