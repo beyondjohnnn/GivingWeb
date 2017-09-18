ea7a464488f76a16e7eec636d233c4ed901e775f
FALLENau <reece.c.jones@gmail.com>
2017-09-18 18:59:36 +0100

    setup seeds gp

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,3 +1,5 @@
+Users.create(first_name: 'Reece', last_name: 'Jones', password: '123')
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
