b407d61d5d06c15cf731cd1949df44fabdfb3ad1
FALLENau <reece.c.jones@gmail.com>
2017-09-19 22:20:09 +0100

    donations setup

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # get 'charities' => 'charities#index'
 
   namespace :v1 do
-    resources :users
-    resources :charities
+    resources :users, :charities, :donations
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918165548) do
+ActiveRecord::Schema.define(version: 20170919210405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20170918165548) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "donations", force: :cascade do |t|
+    t.string "donation_from"
+    t.decimal "donation_amount"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_donations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -30,4 +39,5 @@ ActiveRecord::Schema.define(version: 20170918165548) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "donations", "users"
 end
