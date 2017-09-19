19208bfdffeb934b2a473fd87c3d6f43b8b29516
eoghanscrowley <eoghanscrowley@gmail.com>
2017-09-19 09:39:39 +0100

    add basic styling to nav

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
+import { Link } from 'react-router-dom'
+
+import css from './Navigation.scss'
 
 class Navigation extends React.Component {
 	render() {
 		return (
 			<div className="navigation">
-
-			Hello World!
+				<h1 className="banner">
+					GivingWeb
+				</h1>
+				<form>
+					<input type="text"/>
+				</form>
+				<nav>
+					<Link to="/login" className="login-link">
+						Login
+					</Link>
+					<Link to="/signup" className="signup-link">
+						Signup
+					</Link>
+					<button>Menu</button>
+				</nav>
 			</div>
 		)
 	}
