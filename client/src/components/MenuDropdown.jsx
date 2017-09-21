14017bc001833c04e2a9f7bb40e50f4047538222
Johnny Watson <johnny@streetchange.org.uk>
2017-09-21 15:31:39 +0100

    Amend hideDropdownnOnClick to exclude window coords whre menu button exists

--- client/src/components/MenuDropdown.jsx
+++ client/src/components/MenuDropdown.jsx
@@ -19,8 +19,12 @@ class MenuDropdown extends React.Component {
 
   hideDropdownOnClick(e) {
     const componentBoundaries = this.refs.container.getBoundingClientRect()
-    // console.log(componentBoundaries)
-    if ((e.x < componentBoundaries.left || e.x > componentBoundaries.right || e.y < componentBoundaries.top || e.y > componentBoundaries.bottom) && this.props.dropdownVisible) {
+    const menuButtonBoundaries = document.querySelector('.menu-button').getBoundingClientRect()
+    const outsideDropdown = (e.x < componentBoundaries.left || e.x > componentBoundaries.right || e.y < componentBoundaries.top || e.y > componentBoundaries.bottom)
+    const outsideMenuButton = (e.x < menuButtonBoundaries.left || e.x > menuButtonBoundaries.right || e.y < menuButtonBoundaries.top || e.y > menuButtonBoundaries.bottom)
+    
+
+    if (outsideDropdown && outsideMenuButton && this.props.dropdownVisible) {
         console.log('in hideDropdownOnClick function')
         this.props.toggleDropdown()
     }
