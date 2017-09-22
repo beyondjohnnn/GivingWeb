eae1dc4e59942715a015f1fc7a3829855552cba9
Johnny Watson <johnny@streetchange.org.uk>
2017-09-22 14:04:25 +0100

    Create and build out How It Works component and styling

---
+++ client/src/components/HowItWorks.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+import css from './HowItWorks.scss'
+
+class HowItWorks extends React.Component {
+	render() {
+		return (
+			<div className="how-it-works">
+				<button><i class="fa fa-times" aria-hidden="true"></i></button>
+			</div>
+		)
+	}
+}
+
+export default HowItWorks
