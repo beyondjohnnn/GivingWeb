cbad5ba39618c988efb4326463409d3b46d504fb
eoghanscrowley <eoghanscrowley@gmail.com>
2017-09-22 18:11:51 +0100

    create header for CharityPage

--- client/src/containers/CharityPage.jsx
+++ client/src/containers/CharityPage.jsx
@@ -6,7 +6,17 @@ class CharityPage extends React.Component {
 	render() {
 		return (
 			<div className="charity-page">
-				Charity Page
+				<div className="charity-header">
+					<img src="http://via.placeholder.com/350x350" />
+					<div className="charity-summary">
+						<h2 className="charity-name">Streetwork UK</h2>
+						<p className="summary">We support people who are homeless or at risk of homelessness. We want people to find their own solutions: ‘your terms, your pace, your place’.</p>
+					</div>
+					<div className="charity-actions">
+						<button>Fundraise</button>
+						<button>Donate</button>
+					</div>
+				</div>
 			</div>
 		)
 	}
