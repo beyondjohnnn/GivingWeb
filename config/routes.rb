cde568d78cbe62a58dc1d9bbf497e2af379d109f
eoghanscrowley <eoghanscrowley@gmail.com>
2017-09-26 13:46:07 +0100

    update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :charities
-  devise_for :users
+	devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
+  devise_for :charities, :controllers => {sessions: 'sessions', registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'charities' => 'charities#index'
+
+  resources :sessions, only: [:create, :destroy]
 end
