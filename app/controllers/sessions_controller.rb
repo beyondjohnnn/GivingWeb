fcbc9b0d5e23eef1556f4c013752c6673f485a7d
eoghanscrowley <eoghanscrowley@gmail.com>
2017-09-26 13:51:46 +0100

    add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  respond_to :json
+
+  def create
+    user = User.find_for_database_authentication(email: params[:email])
+    if user.valid_password?(params[:password])
+      render json: token_hash(user)
+    else
+      render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
+    end
+  end
+
+end
