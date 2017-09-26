c0baa73108811ebccd4a8a9843df9405884849bd
eoghanscrowley <eoghanscrowley@gmail.com>
2017-09-26 13:49:37 +0100

    add registrations controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,13 @@
+class RegistrationsController < ApplicationController
+  respond_to :json
+
+  def create
+    if(User.exists?(email: params[:email]))
+      render json: {response: "user exists"}
+    else
+      user = User.create({first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password]})
+      render json: token_hash(user)
+    end
+  end
+
+end
