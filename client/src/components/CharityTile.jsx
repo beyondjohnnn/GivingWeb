6462c45563c5ca8490342412f0c3695f92e8d09d
James Cox <j-cox03@hotmail.com>
2017-09-27 16:10:09 +0100

    added image for user and h3 tag for user name to charity tile

--- client/src/components/CharityTile.jsx
+++ client/src/components/CharityTile.jsx
@@ -6,7 +6,14 @@ class CharityTile extends React.Component {
 
 	render() {
 		return (
-      <div className="charity-tile"></div>
+      <div className="charity-tile">
+        <section className="tile-header">
+          <img className="user-icon_img" src="http://via.placeholder.com/50x50" alt="user icon"/>
+          <div>
+            <h3>User Name</h3>
+          </div>
+        </section>
+      </div>
 		)
 	}
 }
