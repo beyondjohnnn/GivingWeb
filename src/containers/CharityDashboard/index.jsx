7c610710519906f76534956b8e73b59e4b3a7eaf
eoghanscrowley <eoghanscrowley@gmail.com>
2017-10-03 10:38:09 +0100

    create CharityDashboard container

---
+++ src/containers/CharityDashboard/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import css from './CharityDashboard.scss'
+
+class CharityDashboard extends React.Component {
+	render() {
+		return (
+			<div className="charity-dashboard">
+				
+			</div>
+		)
+	}
+}
+
+
+export default CharityDashboard
