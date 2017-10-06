75f3a34ad8d5cfc7847898d45c71a7ecb2c5860e
Johnny Watson <johnny@streetchange.org.uk>
2017-10-06 11:21:21 +0100

    Featured Members changed

--- src/components/FeaturedMembers/index.jsx
+++ src/components/FeaturedMembers/index.jsx
@@ -19,7 +19,7 @@ class FeaturedMembers extends React.Component {
 	}
 
 	prepareMembers(){
-		let members = [this.props.members[0], this.props.members[1], this.props.members[4]];
+		let members = [this.props.members[20], this.props.members[4], this.props.members[19]];
 		for(let index in members){
 			if(!members[index]) return [];
 		}
