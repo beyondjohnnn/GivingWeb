4d8113606e52bff337d50e0c26f61c8fd2021d27
eoghanscrowley <eoghanscrowley@gmail.com>
2017-10-06 15:05:21 +0100

    refactor donation functions

--- src/components/MemberPreview/index.jsx
+++ src/components/MemberPreview/index.jsx
@@ -1,46 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { calcDonationPercentage, getDonationBarColour } from '../../utils/donations'
 
 import css from './MemberPreview.scss'
 
 class MemberPreview extends React.Component {
-
-	devMember(member){
-
-		if(!member){
-			member = {
-				name: "John Smith",
-				location: "North Pole",
-				snippet: "Please send help I'm cold",
-				"donations": 99.9,
-				"goal": 100
-			}
-		}
-
-		if(!member.donations){
-			member.donations = 95.5;
-		}
-
-		return member
-	}
-
-	calcDonationPercentage(member){
-		const donations = member.donations.map((donation) => {
-			return donation.total
-		}).reduce((a, b) => {
-			return a + b
-		}, 0)
-		let donationPercent = (donations / member.goal) * 100;
-		if(donationPercent >= 100) return Math.floor(donationPercent);
-		if(donationPercent < 99) return Math.ceil(donationPercent);
-		else return 99;
-	}
-
-	getDonationBarColour(percentage){
-		if(percentage <= 33) return "#FBAC3D";
-		else if(percentage <= 66) return "#00CB9B";
-		else return "#00862C";
-	}
 
 	createCompletedBanner(percentage) {
 		if (percentage >= 100) {
@@ -49,17 +13,12 @@ class MemberPreview extends React.Component {
 	}
 
 	render() {
-
 		let previewStyle = this.props.style
 		if(!previewStyle) previewStyle = {}
-
-		let member = this.devMember(this.props.member)
-		let imgUrl = "images/" + member.name + ".png"
-		let placeholder = "http://via.placeholder.com/350x350"
-
-		const percentage = this.calcDonationPercentage(member)
+		const {member} = this.props
+		const percentage = calcDonationPercentage(member)
 		const textPercentage = percentage.toString() + "%"
-		const donationBarColour = this.getDonationBarColour(percentage)
+		const donationBarColour = getDonationBarColour(percentage)
 		const donationBarStyles = {
 			"width": textPercentage,
 			"backgroundColor": donationBarColour
@@ -69,7 +28,7 @@ class MemberPreview extends React.Component {
 			<div className="member-preview" style={previewStyle}>
 				<div className="member-photo-container">
 					{this.createCompletedBanner(percentage)}
-					<Link className="member-link" to={`/member?member_id=${member.id}`}><img className="member-photo" src={imgUrl} /></Link>
+					<Link className="member-link" to={`/member?member_id=${member.id}`}><img className="member-photo" src={`images/${member.name}.png`} /></Link>
 					}
 				</div>
 				<div className="member-information">
@@ -77,7 +36,7 @@ class MemberPreview extends React.Component {
 					<h4 className="member-location"><i className="fa fa-map-marker"></i>{member.location}</h4>
 					<p className="member-description">{member.snippet}</p>
 					<h4 className="member-verification">
-						<i className="fa fa-check-circle-o"></i>
+						<i className="fa fa-check-circle-o" />
 						Verified by <Link className="charity-link" to="charity-page">Streetwork</Link>
 					</h4>
 					<div className="donation-details">
---
+++ src/utils/donations.js
@@ -0,0 +1,17 @@
+export function calcDonationPercentage(member){
+	const donations = member.donations.map((donation) => {
+		return donation.total
+	}).reduce((a, b) => {
+		return a + b
+	}, 0)
+	let donationPercent = (donations / member.goal) * 100;
+	if(donationPercent >= 100) return Math.floor(donationPercent);
+	if(donationPercent < 99) return Math.ceil(donationPercent);
+	else return 99;
+}
+
+export function getDonationBarColour(percentage){
+	if(percentage <= 33) return "#FBAC3D";
+	else if(percentage <= 66) return "#00CB9B";
+	else return "#00862C";
+}
