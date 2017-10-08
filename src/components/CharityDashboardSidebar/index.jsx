a2658d8baf78168195a946e14372c34b2cd4af38
Johnny Watson <johnny@streetchange.org.uk>
2017-10-08 13:10:24 +0100

    Style selected tab

--- src/components/CharityDashboardSidebar/index.jsx
+++ src/components/CharityDashboardSidebar/index.jsx
@@ -12,22 +12,22 @@ class CharityDashboardSidebar extends React.Component {
 			<div className="charity-dashboard-sidebar">
 				<CharityDashboardOrganisationDetails />
 				<div className="charity-sidebar-navigation">
-					<Link	className="charity-sidebar-link" to="">
+					<Link	className="charity-sidebar-link" to="charity-dashboard">
 						<i className="fa fa-rocket" /> Campaigns
 					</Link>
-					<Link	className="charity-sidebar-link"	to="">
+					<Link	className="charity-sidebar-link selected-tab"	to="charity-dashboard">
 						<i className="fa fa-users" /> Members
 					</Link>
-					<Link	className="charity-sidebar-link"	to="">
+					<Link	className="charity-sidebar-link"	to="charity-dashboard">
 						<i className="fa fa-bank" /> Main Fund
 					</Link>
-					<Link	className="charity-sidebar-link"	to="">
+					<Link	className="charity-sidebar-link"	to="charity-dashboard">
 						<i className="fa fa-sitemap" /> My Organisation
 					</Link>
-					<Link	className="charity-sidebar-link"	to="">
+					<Link	className="charity-sidebar-link"	to="charity-dashboard">
 						<i className="fa fa-question" /> Help
 					</Link>
-					<Link	className="charity-sidebar-link"	to="">
+					<Link	className="charity-sidebar-link"	to="charity-dashboard">
 						<i className="fa fa-line-chart" /> Reporting
 					</Link>
 				</div>
