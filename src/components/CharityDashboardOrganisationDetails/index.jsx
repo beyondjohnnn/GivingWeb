a50d75a2332e5448048aff31260eeed0b6a0fad0
Johnny Watson <johnny@streetchange.org.uk>
2017-10-08 12:44:17 +0100

    Build out organisation details component

--- src/components/CharityDashboardOrganisationDetails/index.jsx
+++ src/components/CharityDashboardOrganisationDetails/index.jsx
@@ -6,8 +6,11 @@ class CharityDashboardOrganisationDetails extends React.Component {
 	render() {
 		return (
 			<div className="charity-dashboard-organisation-details">
-				<img src="images/streetwork.jpg" />
-				<h3>Streetwork</h3>
+        <img src="images/streetwork.jpg" />
+				<div className="banner-image"></div>
+        <h3>Streetwork</h3>
+        <p className="amount-raised">£3,300</p>
+				<p className="amount-raised-text">Total Raised</p>
 			</div>
 		)
 	}
