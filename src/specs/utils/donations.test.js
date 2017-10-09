b55a4b722682fe4c1910dd9748097e07ea47975f
James Cox <j-cox03@hotmail.com>
2017-10-09 12:16:42 +0100

    made tests for method calcDonationPercentage

--- src/specs/utils/donations.test.js
+++ src/specs/utils/donations.test.js
@@ -1,21 +1,25 @@
 import { calcTotalDonations, calcDonationPercentage, getDonationBarColour } from './../../utils/donations'
 
-describe('donation functions', () => {
-	let member
+let member
+function defaultMember(){
+	return {
+		goal: 50,
+		donations: [
+			{total: 2},
+			{total: 7},
+			{total: 3},
+			{total: 8},
+		],
+		comments: [],
+		info: '',
+		name: 'Charlie'
+	}
+}
+
+describe('calcTotalDonations', () => {
 
 	beforeEach(() => {
-		member = {
-			goal: 50,
-      donations: [
-				{total: 2},
-				{total: 7},
-				{total: 3},
-				{total: 8},
-			],
-      comments: [],
-      info: '',
-      name: 'Charlie'
-    }
+		member = defaultMember()
 	});
 
 	it('should be able to calculate the total donations of a member', () => {
@@ -31,6 +35,40 @@ describe('donation functions', () => {
 
 		const result = calcTotalDonations(member)
 		const expected = 50
+		expect(result).toBe(expected)
+	})
+})
+
+describe('calcDonationPercentage', () => {
+
+	beforeEach(() => {
+		member = defaultMember()
+	});
+
+	it('should be able to calculate the donation of a member', () => {
+		const result = calcDonationPercentage(member)
+		const expected = 40
+		expect(result).toBe(expected)
+	})
+
+	it('should always round up if donation percentage is less than 99', () => {
+		let result = calcDonationPercentage(member)
+		let expected = 41
+
+		member.donations.push({total: 0.125})
+		result = calcDonationPercentage(member)
+		expect(result).toBe(expected)
+
+		member.donations.pop()
+
+		member.donations.push({total: 0.25})
+		result = calcDonationPercentage(member)
+		expect(result).toBe(expected)
+
+		member.donations.pop()
+
+		member.donations.push({total: 0.375})
+		result = calcDonationPercentage(member)
 		expect(result).toBe(expected)
 	})
 })
