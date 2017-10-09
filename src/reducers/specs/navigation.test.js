798bf009b17fd03d5087929148501dce5df1c531
James Cox <j-cox03@hotmail.com>
2017-10-09 09:59:31 +0100

    made tests for navigation reducer

---
+++ src/reducers/specs/navigation.test.js
@@ -0,0 +1,40 @@
+import navigationReducer from './../navigation'
+
+
+
+describe('CharityTile', () => {
+	let defaultState
+
+	beforeEach(() => {
+		defaultState = {dropdownVisible: false}
+	});
+
+	it('should return default state when passed undefined and no action type', () => {
+		const result = navigationReducer(undefined, {})
+		const expected = {
+			dropdownVisible: false
+		}
+		expect(result).toMatchObject(expected)
+	})
+
+	it('should toggle value of "dropdownVisible" when action "TOGGLE_DROPDOWN" is passed', () => {
+		const action = {
+			type: "TOGGLE_DROPDOWN"
+		}
+		let newState
+		let expectedState
+
+		newState = navigationReducer(defaultState, action)
+		expectedState = {
+			dropdownVisible: true
+		}
+		expect(newState).toMatchObject(expectedState)
+
+		newState = navigationReducer(newState, action)
+		expectedState = {
+			dropdownVisible: false
+		}
+		expect(newState).toMatchObject(expectedState)
+	})
+
+})
