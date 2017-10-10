3d188b7eb548b814c977e74a5bd2a590fecf9ca6
eoghanscrowley <eoghanscrowley@gmail.com>
2017-10-10 21:45:09 +0100

    fix overflow scroll on new member process

--- src/components/NewMemberFormCoverPhoto/index.jsx
+++ src/components/NewMemberFormCoverPhoto/index.jsx
@@ -6,7 +6,10 @@ class NewMemberFormCoverPhoto extends React.Component {
 	render() {
 		return (
 			<div className="new-member-form-cover-photo">
-				This is the cover photo form
+				<img src="http://via.placeholder.com/300x300" alt=""/>
+				<div className="controls">
+					<p>Please pick a nice photo. This will be the photo that potential doners will see.</p>
+				</div>
 			</div>
 		)
 	}
