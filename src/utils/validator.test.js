d17d824a3553699864eb770ba6d0e4a0471a0f04
James Cox <j-cox03@hotmail.com>
2017-10-11 17:46:17 +0100

    Made validator methods

--- src/utils/validator.test.js
+++ src/utils/validator.test.js
@@ -1,5 +1,5 @@
 
-import { isStringValid } from './validator'
+import { isStringValid, isMonetaryValueValid } from './validator'
 
 describe('Validator', () => {
 
@@ -13,6 +13,26 @@ describe('Validator', () => {
 
   it('should be able to check if a string is not valid (empty string)', () => {
     expect(isStringValid("")).toBe(false)
+	})
+
+  it('should be able to check if a number representing money is valid', () => {
+    expect(isMonetaryValueValid(0)).toBe(true)
+    expect(isMonetaryValueValid(10.55)).toBe(true)
+    expect(isMonetaryValueValid(999999)).toBe(true)
+	})
+
+  it('should be able to check if a number representing money is not valid (null input)', () => {
+    expect(isMonetaryValueValid(null)).toBe(false)
+	})
+
+  it('should be able to check if a number representing money is not valid (negative input)', () => {
+    expect(isMonetaryValueValid(-1)).toBe(false)
+    expect(isMonetaryValueValid(-100)).toBe(false)
+    expect(isMonetaryValueValid(-999999)).toBe(false)
+	})
+
+  it('should be able to check if a number representing money is not valid (decimal to 3dp or more input)', () => {
+    expect(isMonetaryValueValid(10.555)).toBe(false)
 	})
 
 });
