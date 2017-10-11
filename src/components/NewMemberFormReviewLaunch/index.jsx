2062af41c8223db4dbdf400cee9a8b91f26647eb
eoghanscrowley <eoghanscrowley@gmail.com>
2017-10-11 14:16:01 +0100

    add review and launch section

--- src/components/NewMemberFormReviewLaunch/index.jsx
+++ src/components/NewMemberFormReviewLaunch/index.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+
+import * as actionCreators from '../../actions/newMemberProcess'
 
 import css from './NewMemberFormReviewLaunch.scss'
 
@@ -6,10 +10,36 @@ class NewMemberFormReviewLaunch extends React.Component {
 	render() {
 		return (
 			<div className="new-member-form-review-launch">
-				This is the review & launch form
+				<img src={this.props.imagePreviewUrl} />
+
+				<h3>Name</h3>
+				<p>{this.props.member_name}</p>
+
+				<h3>Fundraising Goal</h3>
+				<p>£ {this.props.goal}</p>
+
+				<h3>Location</h3>
+				<p>{this.props.location}</p>
+
+				<h3>Reason for use</h3>
+				<p>{this.props.story.reason_for_use}</p>
+
+				<h3>Story</h3>
+				<p>{this.props.story.story}</p>
+
+				<h3>Future goals</h3>
+				<p>{this.props.story.future_goals}</p>
 			</div>
 		)
 	}
 }
 
-export default NewMemberFormReviewLaunch
+function mapStateToProps(state, routing) {
+	return { ...state.newMemberProcess, ...routing }
+}
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators(actionCreators, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewMemberFormReviewLaunch)
