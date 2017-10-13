17a55b178d07cdacfb2d6b2b68a589b13184a1db
James Cox <j-cox03@hotmail.com>
2017-10-13 23:56:50 +0100

    made script for selecting featured members

---
+++ src/utils/scripts/featured_members.js
@@ -0,0 +1,62 @@
+const axios = require('axios')
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function getUserInput(prompt) {
+  const promise = new Promise((resolve, reject) => {
+    rl.question(`${prompt}:\n`, function (input) {
+      resolve(input)
+    })
+  })
+  return promise
+}
+
+let allMembers
+let selectedMembers = []
+
+axios.get("http://localhost:3000/members")
+  .then((response) => {
+
+    allMembers = response.data
+    for(let index in allMembers){
+      const num = parseInt(index) + 1
+      console.log(`${num}: ${allMembers[index].name}`)
+    }
+
+    return getUserInput("Enter the number of a member you wish to make featured")
+  })
+  .then((input) => {
+    input = parseInt(input)
+    selectedMembers.push(allMembers[input-1])
+    console.log("You select " + allMembers[input-1].name);
+    return getUserInput("Please select another")
+  }).then((input) => {
+    input = parseInt(input)
+    selectedMembers.push(allMembers[input-1])
+    console.log("You select " + allMembers[input-1].name);
+    return getUserInput("Please select another")
+  }).then((input) => {
+    input = parseInt(input)
+    selectedMembers.push(allMembers[input-1])
+    console.log("You select " + allMembers[input-1].name);
+    return selectedMembers
+  }).then((selectedMembers) => {
+    let promises = []
+    let position = 1
+    for(member of selectedMembers){
+      const toPost = {id: member.id, position: position++}
+      promises.push(axios.post("http://localhost:3000/featured_members", toPost))
+    }
+    return Promise.all(promises)
+  }).then((result) => {
+    console.log("Fetured members have been selected")
+    process.exit()
+  })
+  .catch((error) => {
+    console.log("error: ", error)
+    process.exit()
+  })
