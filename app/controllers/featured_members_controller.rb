faa340e2e77fb07c01b934330e8ddd928f71f1d2
James Cox <j-cox03@hotmail.com>
2017-10-13 17:48:07 +0100

    featured_members_controller: added to routes index and create

--- app/controllers/featured_members_controller.rb
+++ app/controllers/featured_members_controller.rb
@@ -1,11 +1,25 @@
 class FeaturedMembersController < ApplicationController
 
+  # Account.includes(:details).where(:id => current_account_id).first
+
     def index
-      render json: {route: "index"}
+      featured_members = FeaturedMember.order(:position)
+      members = featured_members.map do |featured_member|
+        {
+          member: Member.where(id: featured_member.member_id)[0],
+          position: featured_member.position
+        }
+      end
+      render json: members
     end
 
     def create
-      render json: {route: "create", posted: params[:id]}
+      featured_member = FeaturedMember.new({member_id: params[:id], position: params[:position]})
+      if featured_member.save
+        render json: featured_member
+      else
+        render json: {errors: "did not save"}
+      end
     end
 
     def destroy
