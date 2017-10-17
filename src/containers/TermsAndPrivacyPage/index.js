65742a7a8599d6c28e6a3784e878fa6f59898fb1
eoghanscrowley <eoghanscrowley@gmail.com>
2017-10-17 15:52:03 +0100

    fix bug

--- src/containers/TermsAndPrivacyPage/index.js
+++ src/containers/TermsAndPrivacyPage/index.js
@@ -4,9 +4,6 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser'
 
-
-import TermsAndPrivacy from '../../components/TermsAndPrivacy'
-
 import css from './TermsAndPrivacyPage.scss'
 
 class TermsAndPrivacyPage extends React.Component {
