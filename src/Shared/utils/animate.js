fb64fa792ce5dd5ddcca83dce8a6979d6debc76c
James Cox <j-cox03@hotmail.com>
2017-10-23 22:53:58 +0100

    Animate.js: used es6 class

--- src/Shared/utils/animate.js
+++ src/Shared/utils/animate.js
@@ -1,30 +1,33 @@
 
-function Animation(framesPerSecond, frameCallBack){
-  this.interval = 1000/framesPerSecond;
-  this.startTime = 0;
-  this.frameCallBack = frameCallBack;
-  this.run = this.run.bind(this)
-}
+class Animation{
 
-Animation.prototype.start = function(){
-  this.startTime = Date.now();
-  requestAnimationFrame(this.run);
-}
-
-Animation.prototype.run = function(){
-  let runNext = true
-  if(this.shouldRunNextFrame()){
-    runNext = this.frameCallBack();
+  constructor(framesPerSecond, frameCallBack){
+    this.interval = 1000/framesPerSecond;
+    this.startTime = 0;
+    this.frameCallBack = frameCallBack;
+    this.run = this.run.bind(this)
   }
-  if(runNext)requestAnimationFrame(this.run);
-}
 
-Animation.prototype.shouldRunNextFrame = function(){
-  let now = Date.now();
-  let elapsed = now - this.startTime;
-  var shouldRun = elapsed >= this.interval;
-  if(shouldRun) this.startTime = now;
-  return shouldRun;
+  start(){
+    this.startTime = Date.now();
+    requestAnimationFrame(this.run);
+  }
+
+  run(){
+    let runNext = true
+    if(this.shouldRunNextFrame()){
+      runNext = this.frameCallBack();
+    }
+    if(runNext)requestAnimationFrame(this.run);
+  }
+
+  shouldRunNextFrame(){
+    let now = Date.now();
+    let elapsed = now - this.startTime;
+    var shouldRun = elapsed >= this.interval;
+    if(shouldRun) this.startTime = now;
+    return shouldRun;
+  }
 }
 
 export default Animation
