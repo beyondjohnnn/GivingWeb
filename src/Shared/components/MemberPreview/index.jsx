e3195038344a386c9cfc25417fa9de25b18bfb63
James Cox <j-cox03@hotmail.com>
2017-10-23 17:03:29 +0100

    merged

--- src/Shared/components/MemberPreview/index.jsx
+++ src/Shared/components/MemberPreview/index.jsx
@@ -92,11 +92,7 @@ class MemberPreview extends React.Component {
 								<p className="goal-label">GOAL</p>
 							</div>
 						</div>
-<<<<<<< HEAD
 						{this.renderDonateButton(this.hasSponsor ? percentage*2 : percentage)}
-=======
-						{this.renderDonateButton(percentage}
->>>>>>> development
 						<DonationProgressBar percentage={percentage} hasSponsor={this.hasSponsor} />
 					</div>
 					<div className="member-donations">Donations</div>
