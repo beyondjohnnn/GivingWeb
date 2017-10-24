d6817670b10fb16b74eb2f73875136e1cf438856
eoghanscrowley <eoghanscrowley@gmail.com>
2017-10-24 10:09:21 +0100

    update collected details from fb auth

--- src/UserSignup/components/LoginButton/index.jsx
+++ src/UserSignup/components/LoginButton/index.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import { FacebookLogin } from 'react-facebook-login-component'
+import FacebookProvider, {Login} from 'react-facebook'
 
 import css from './LoginButton.scss'
 
@@ -10,25 +11,44 @@ class LoginButton extends Component {
   }
  
   responseFacebook (response) {
-    console.log(response);
-    //anything else you want to do(save to localStorage)...
+    const details = {
+      first_name: response.first_name,
+      last_name: response.last_name,
+      email: response.email,
+      gender: response.gender,
+      location: response.location.name,
+      url_image: response.picture.data.url,
+      fb_token: response.accessToken
+    }
+    console.log(details);
   }
 
 	render() {
 		return (
 			<div className="login-button">
-				<FacebookLogin socialId="184647528774656"
+        <FacebookLogin socialId="184647528774656"
           language="en_US"
           scope="public_profile,email,user_location,user_birthday,user_photos"
           responseHandler={this.responseFacebook}
           xfbml={true}
-          fields="id,email,first_name,last_name,picture,age_range,birthday,currency,gender,hometown,location,locale,family"
+          fields="id,email,first_name,last_name,picture,birthday,currency,gender,hometown,location,locale,family"
           version="v2.10"
           className="facebook-login"
           buttonText="Login With Facebook"/>
-			</div>
-		)
-	}
+      </div>
+    )
+  }
 }
 
 export default LoginButton
+        // <FacebookProvider appId="184647528774656">
+        //   <Login
+        //     scope="public_profile,user_location,user_birthday,user_photos"
+        //     responseHandler={this.responseFacebook}
+        //     xfbml={true}
+        //     fields={['email','first_name','last_name','picture','birthday','currency','gender','hometown','location','locale','family']}
+        //     version="v2.10"
+        //     className="facebook-login">
+        //     Login With Facebook
+        //   </Login>
+        // </FacebookProvider>
