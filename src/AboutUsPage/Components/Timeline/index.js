945a17d637de8b94366f59f6fc028552b88ee917
Johnny Watson <johnny@streetchange.org.uk>
2017-10-30 19:00:00 +0000

    Build out the about us section

--- src/AboutUsPage/Components/Timeline/index.js
+++ src/AboutUsPage/Components/Timeline/index.js
@@ -10,7 +10,41 @@ class Timeline extends React.Component {
     return (
       <div className="timeline">
         <div className="timeline-header"></div>
-        <div className="main-wrapper"></div>
+        <div className="main-wrapper">
+          <div className="timeline-block"></div> 
+          <div className="timeline-wrapper">
+            <div className="timeline-asset">
+              <div className="asset-arrow"></div>
+              <div className="timeline-content">
+              <h4 className="timeline-title">November 2015</h4>
+              Our CEO Johnny took part in the Product Forge accelerator back in November '15, where he developed a concept for a point of sale technology.  This was aimed at supporting homeless people and thus his enthusiasm for driving social change with tech was born.
+              </div>
+              <div className="image-cover"> 
+                <img src={"https://pbs.twimg.com/profile_images/717306100312383488/f38LU5Cu.jpg"} alt=""/>
+              </div>
+            </div>
+            <div className="timeline-asset">
+              <div className="asset-arrow"></div>
+              <div className="image-cover"> 
+                <img src={"https://pbs.twimg.com/profile_images/717306100312383488/f38LU5Cu.jpg"} alt=""/>
+              </div>
+              <div className="timeline-content">
+              <h4 className="timeline-title">November 2015</h4>
+              Our CEO Johnny took part in the Product Forge accelerator back in November '15, where he developed a concept for a point of sale technology.  This was aimed at supporting homeless people and thus his enthusiasm for driving social change with tech was born.
+              </div>
+            </div>
+            <div className="timeline-asset">
+              <div className="asset-arrow"></div>
+              <div className="timeline-content">
+              <h4 className="timeline-title">November 2015</h4>
+              Our CEO Johnny took part in the Product Forge accelerator back in November '15, where he developed a concept for a point of sale technology.  This was aimed at supporting homeless people and thus his enthusiasm for driving social change with tech was born.
+              </div>
+              <div className="image-cover"> 
+                <img src={"https://pbs.twimg.com/profile_images/717306100312383488/f38LU5Cu.jpg"} alt=""/>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
