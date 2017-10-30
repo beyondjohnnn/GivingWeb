10cc0baf9805e08cb92370bce4217f688b0b8a22
Johnny Watson <johnny@streetchange.org.uk>
2017-10-30 21:49:02 +0000

    Finish building out structure and styling of timeline

--- src/AboutUsPage/Components/Timeline/index.js
+++ src/AboutUsPage/Components/Timeline/index.js
@@ -13,31 +13,61 @@ class Timeline extends React.Component {
         <div className="main-wrapper">
           <div className="timeline-block"></div> 
           <div className="timeline-wrapper">
+          <div className="timeline-asset">
+            <div className="asset-arrow"></div>
+            <div className="timeline-content">
+            <h4 className="timeline-title">November 2017</h4>
+            We rebuild and rebrand our platform and set off on our mission to support charities with the tools and digital strategy needed to rocket launch online fundraising and engagement.
+            </div>
+            <div className="image-cover giving-web-image"> 
+              <img src={"https://n6-img-fp.akamaized.net/free-vector/startup-rocket-launch_23-2147504814.jpg?size=338&ext=jpg&ve=1"} alt=""/>
+            </div>
+          </div>
             <div className="timeline-asset">
               <div className="asset-arrow"></div>
               <div className="timeline-content">
-              <h4 className="timeline-title">November 2015</h4>
-              Our CEO Johnny took part in the Product Forge accelerator back in November '15, where he developed a concept for a point of sale technology.  This was aimed at supporting homeless people and thus his enthusiasm for driving social change with tech was born.
+              <h4 className="timeline-title">April 2017</h4>
+              Johnny immerses himself in 16 weeks of software development training at CodeClan and assembles a superstar team to rebuild the platform.  
               </div>
               <div className="image-cover"> 
-                <img src={"https://pbs.twimg.com/profile_images/717306100312383488/f38LU5Cu.jpg"} alt=""/>
+                <img src={"https://pbs.twimg.com/profile_images/606015765700091905/dv5RE0l9_400x400.jpg"} alt=""/>
               </div>
             </div>
             <div className="timeline-asset">
               <div className="asset-arrow"></div>
               <div className="image-cover"> 
-                <img src={"https://pbs.twimg.com/profile_images/717306100312383488/f38LU5Cu.jpg"} alt=""/>
+                <img src={"/images/streetchangelogo.png"} alt=""/>
               </div>
               <div className="timeline-content">
-              <h4 className="timeline-title">November 2015</h4>
-              Our CEO Johnny took part in the Product Forge accelerator back in November '15, where he developed a concept for a point of sale technology.  This was aimed at supporting homeless people and thus his enthusiasm for driving social change with tech was born.
+              <h4 className="timeline-title">February 2017</h4>
+              We launch the beta of our platform and begin our pilot with Streetwork. We help support 20 people to share their story and raise money for basic needs.
+              </div>
+            </div>
+            <div className="timeline-asset">
+              <div className="asset-arrow"></div>
+              <div className="image-cover giving-web-image"> 
+                <img src={"/images/civtech.png"} alt=""/>
+              </div>
+              <div className="timeline-content">
+              <h4 className="timeline-title">September 2016</h4>
+              We are invited to participate in the CivTech accelerator in CodeBase, Edinburgh and join the first ever cohort.  
+              </div>
+            </div>
+            <div className="timeline-asset">
+              <div className="asset-arrow"></div>
+              <div className="timeline-content">
+              <h4 className="timeline-title">March 2016</h4>
+              We form a close partnership with local homelessness charity Streetwork and begin planning the pilot of our member fundraising. 
+              </div>
+              <div className="image-cover"> 
+                <img src={"./images/streetwork.jpg"} alt=""/>
               </div>
             </div>
             <div className="timeline-asset">
               <div className="asset-arrow"></div>
               <div className="timeline-content">
               <h4 className="timeline-title">November 2015</h4>
-              Our CEO Johnny took part in the Product Forge accelerator back in November '15, where he developed a concept for a point of sale technology.  This was aimed at supporting homeless people and thus his enthusiasm for driving social change with tech was born.
+              Johnny first discovers his desire to drive social change with tech and wins a hackathon with a solution aimed at tackling homelessness.
               </div>
               <div className="image-cover"> 
                 <img src={"https://pbs.twimg.com/profile_images/717306100312383488/f38LU5Cu.jpg"} alt=""/>
